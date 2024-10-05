2e1c858baa0eb0ae37999a0d10f46f531fa249a4
asraful-07 <asraful0508@gmail.com>
2024-10-05 10:17:26 +0600

    added Emoji

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ const displayVideos = (videos) => {
        <div class="flex items-center gap-1">
        <p class="text-gray-400">${video.authors[0].profile_name}</p>
        ${video.authors[0].verified == true
-        ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" /> ` : ""
+        ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : ""
        }
        </div>
     </div>
